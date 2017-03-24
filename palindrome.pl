069560d817bd7da83d954f837d3387eac6bf1c72
prakasini <prakasini.k424@gmail.com>
2017-03-24 15:24:21 +0530

    palindrome

--- palindrome.pl
+++ palindrome.pl
@@ -1,11 +1,18 @@
 print "\n";
-print "palindrome WORDS";
-print "\n";
+
         for($i=0;$i<=$#ARGV;$i=$i+1)
         {
                 if($ARGV[$i] eq reverse($ARGV[$i]))
-                {
+                {       
+                        print "palindrome WORDS";
+                        print "\n";
                         print "$ARGV[$i] \n";
+                }
+                else
+                {
+                        print "not palindrome WORDS";
+                        print "\n";
+                       print "$ARGV[$i] \n";
                 }
         }
 
