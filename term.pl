151418b33c11b84f4f7507479bf5d8619fc45c57
prakasini <prakasini.k424@gmail.com>
2017-03-24 15:20:49 +0530

    Update term.pl

--- term.pl
+++ term.pl
@@ -4,7 +4,7 @@ open(my $fh,'<:encoding(UTF-8)',$file)or die "could not open filename '$file' $!
 $total=0;
 	while(my $line=<$fh>)
 	{
-	my @match=$line=m/($word)/g;
+	my @match=$line=~m/($word)/g;
 	my $count=scalar(@match);
 	$total=$total+$count;
 	#print "$count\t $line\n";
